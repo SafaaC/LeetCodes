de6bbbfd8011d8e10fbae60760bc27d4299f7a37
SafaaC <safaaacherak@gmail.com>
2023-08-08 13:45:33 +0530

    PS1TwoSum

---
+++ ps1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<target){
+                if(target-nums[i])
+            }
+        }
+            
+        
+        return new int[]{};
+    }
+}
